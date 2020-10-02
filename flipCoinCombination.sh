dae0bb01622b54aded7ed0caa09768c352a8a5bf
rishika7-hub <srishika970@gmail.com>
2020-10-03 00:42:46 +0530

    Added:Welcome message

---
+++ flipCoinCombination.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Welcome to Flip Coin Combination Problem"
+echo "Enter the number of times you would like to flip:"
+read n
+echo "Enter your choice:"
+echo "1.Singlet"
+echo "2.Doublet"
+echo "3.Triplet"
+read option
+
