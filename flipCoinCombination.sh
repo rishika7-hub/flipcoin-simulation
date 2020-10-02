efa439974ccccc2731a6046b95e48c8f880ece7c
rishika7-hub <srishika970@gmail.com>
2020-10-03 01:29:46 +0530

    Added:triplet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 declare -A dict2
+declare -A dict3
 
 echo "Welcome to Flip Coin Combination Problem"
 echo "Enter the number of times you would like to flip:"
@@ -74,3 +75,64 @@ percentage_of_tt=$(( $counttt * 5 ))
 percentage_of_ht=$(( $countht * 5 ))
 percentage_of_th=$(( $countth * 5 ))
 ;;
+
+#hhh-1 ttt-2 hht-3 hth-4 thh-5 tth-6 tht-7 htt-8
+
+"3.")
+for(( i=1; i<=$n; i++ ))
+do
+num1=$(( 1 + $RANDOM % 8 ))
+if [ $num1 -eq 1 ]
+then 
+	echo "HHH"
+	let counthhh++
+elif [ $num1 -eq 2 ]
+	echo "TTT"
+	let countttt++
+elif [ $num1 -eq 3 ]
+then
+	echo "HHT"
+	let counthht++
+elif [ $num1 -eq 4 ]
+then
+	echo "HTH"
+	let counthth++
+elif [ $num1 -eq 5 ]
+then 
+	echo "THH"
+	let countthh++
+elif [ $num1 -eq 6 ]
+then
+	echo "TTH"
+	let counttth++
+elif [ $num1 -eq 7 ]
+then
+	echo "THT"
+	let counttht++
+else [ $num1 -eq 8 ]
+	echo "HTT"
+	let counthtt++
+fi
+done
+
+Triplet[HHH]="$counthhh"
+Triplet[TTT]="$countttt"
+Triplet[HHT]="$counthht"
+Triplet[HTH]="$counthth"
+Triplet[THH]="$countthh"
+Triplet[TTH]="$countthh"
+Triplet[THT]="$counttth"
+Triplet[HTT]="$counthtt"
+
+
+
+percentage_of_hhh=$(( $counthh * 5 ))
+percentage_of_ttt=$(( $counttt * 5 ))
+percentage_of_hht=$(( $countht * 5 ))
+percentage_of_hth=$(( $countth * 5 ))
+percentage_of_thh=$(( $counthh * 5 ))
+percentage_of_tth=$(( $counttt * 5 ))
+percentage_of_tht=$(( $countht * 5 ))
+percentage_of_htt=$(( $counthh * 5 ))
+;;
+esac 
