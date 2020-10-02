f4b42381ff9699eded510b4432abd6f81d849c4e
rishika7-hub <srishika970@gmail.com>
2020-10-03 00:49:56 +0530

    Added: singlet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+declare -A dict1
+
 echo "Welcome to Flip Coin Combination Problem"
 echo "Enter the number of times you would like to flip:"
 read n
@@ -15,4 +18,26 @@ then
 else
 	echo "Tail"
 fi
+
+case "$option" in
+
+"1.")
+for (( i=1; i<=$n; i++ ))
+do
+num=$(( RANDOM % 2 ))
+if [ $num -eq 0 ]
+then
+	echo "Head"
+	let count1++
+else
+	echo "Tail"
+	let count2++
+fi
+done
+dict1[Head]="$count1"
+dict1[Tail]="$count2"
+
+percentage_of_head=$(( $count1 * 5)) 
+percentage_of_tail=$(( $count2 * 5)) 
+;;
 
