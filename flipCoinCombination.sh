1da7af947d7ee8aaba9f0228f80049d6b428ceec
rishika7-hub <srishika970@gmail.com>
2020-10-03 01:27:11 +0530

    Added: Doublet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-declare -A dict1
 declare -A dict2
 
 echo "Welcome to Flip Coin Combination Problem"
@@ -38,8 +36,8 @@ done
 dict1[Head]="$count1"
 dict1[Tail]="$count2"
 
-percentage_of_head=$(( $count1 * 5)) 
-percentage_of_tail=$(( $count2 * 5)) 
+percentage_of_head=$(( $count1 * 5 )) 
+percentage_of_tail=$(( $count2 * 5 )) 
 ;;
 
 #hh-1 tt-2 ht-3 th-4
@@ -66,10 +64,10 @@ else [ $num1 -eq 4 ]
 	let countth++
 fi
 done
-dict2[HH]="$counthh"
-dict2[TT]="$counttt"
-dict2[HT]="$countht"
-dict2[TH]="$countth"
+Doublet[HH]="$counthh"
+Doublet[TT]="$counttt"
+Doublet[HT]="$countht"
+Doublet[TH]="$countth"
 
 percentage_of_hh=$(( $counthh * 5 ))
 percentage_of_tt=$(( $counttt * 5 ))
